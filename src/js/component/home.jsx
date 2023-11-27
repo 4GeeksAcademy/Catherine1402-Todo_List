977b5311834d645232f0c6bd8f8da9bf232809d2
Catherine Mohozzo <144625881+Catherine1402@users.noreply.github.com>
2023-11-27 18:40:39 +0000

    todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [task, setTask] = useState(''); //CajaDeZapatos
+    const [newTasks, setNewTasks] = useState([]); //ropero
+    const [hideButtonDelete, setHideButtonDelete] = useState(null);
+    
+
+
+    const doTask = (e) => {   //Colocar caja de zapatos
+        setTask(e.target.value);
+    };
+
+    const useKeyEnter = (e) => {  // Usar tecla enter para agregar tarea
+        if (e.key === "Enter") {
+            setNewTasks(newTasks.concat([task]));
+            setTask("");
+        }
+    };
+
+    const deleteTask = (id) => {
+        const delet = newTasks.filter((_task, index) => {
+            return index !== id;
+        });
+        setNewTasks(delet);
+    };
+
+    return (
+        <div className="todos position-absolute top-0 start-50 translate-middle">
+            <h1 className="h1 fw-light">todos</h1>
+
+            <ul className="task {task}">
+
+                <input className="input" type="text" placeholder="What needs to be done?" onChange={doTask} value={task} onKeyDown={useKeyEnter} />
+
+                {newTasks.map((task, i) => (
+
+                    <li key={task.i} onMouseEnter={() => setHideButtonDelete(i)} onMouseLeave={() => setHideButtonDelete(null)} className="items {task}">
+                        <span className="textoTarea">{task}</span>
+                        {hideButtonDelete === i && (
+                            <button onClick={() => deleteTask(i)} className="btnDelete">Delete</button>
+                        )}
+                    </li>
+
+                ))}
+                <div className="left"> {newTasks.length} Item left </div>
+
+
+            </ul>
+
+
+        </div>
+    );
 };
 
 export default Home;
